1b1968d807c670251f22af43f7249b697865fec6
Renato Mateus <renatomateusx@gmail.com>
2021-03-30 20:35:38 -0300

    Got started with explore view

--- CloneTwitter/Controllers/ExploreController.swift
+++ CloneTwitter/Controllers/ExploreController.swift
@@ -7,16 +7,31 @@
 
 import UIKit
 
-class ExploreViewController: UIViewController, UIProtocols {
+class ExploreViewController: UITableViewController, UIProtocols {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureUI()
     }
     
     internal func configureUI(){
         view.backgroundColor = .systemBackground
         navigationItem.title = "Explore"
+        tableView.register(UserCell.self, forCellReuseIdentifier: UserCell.identifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+
+extension ExploreViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UserCell.identifier, for: indexPath) as! UserCell
+        cell.configure()
+        return cell
     }
 }
---
+++ CloneTwitter/View/UserCell.swift
@@ -0,0 +1,68 @@
+//
+//  UserCell.swift
+//  CloneTwitter
+//
+//  Created by Renato Mateus on 30/03/21.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    //MARK: Properties
+    static let identifier = "UserCell"
+    
+    static let imageSize: CGFloat = 40
+    private lazy var profileImageView: UIImageView = {
+        let profileImageView = UIImageView()
+        profileImageView.backgroundColor = .twitterBlue
+        profileImageView.setDimensions(width: TweetViewCell.imageSize, height: TweetViewCell.imageSize)
+        profileImageView.layer.cornerRadius = TweetViewCell.imageSize/2
+        profileImageView.layer.masksToBounds = true
+        return profileImageView
+    }()
+    
+    private let usernameLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "@renatomateusx"
+        return label
+    }()
+    
+    private let fullNameLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Renato Mateus"
+        return label
+    }()
+    
+    //MARK: Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .systemBackground
+        configureSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Helpers
+    
+    func configureSubviews(){
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullNameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    func configure(){
+        
+    }
+}
